243342f2e4f0261b426a7fe312eb5fe6c56b7995
Kardnic <158296649+Kardnic@users.noreply.github.com>
2025-11-11 08:35:21 +0100

    test bilder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import HauptLayout from "../layouts/HauptLayout";
 
 export default function Home() {
+  const [fullscreenSrc, setFullscreenSrc] = useState(null);
   return (
     <HauptLayout>
+      
       <h1>Willkommen bei der Hochland Imkerei Heistern</h1>
       <p>
         Es war still im Garten – zu still. Was fehlte, war das vertraute Summen
@@ -30,13 +32,19 @@ export default function Home() {
         Etikett ablösen, spülen, abtrocknen und bei uns wieder abgeben.
       </p>
       <p>
-      <img src="/Bilder/Honigprämierung2025.jpg" alt="Urkunde Honigprämierung 2025" height="50px"/>
+      <img src="/Bilder/Honigprämierung2025.jpg" alt="Urkunde Honigprämierung 2025" height="10%" onClick={() => setFullscreenSrc("/Bilder/Honigprämierung2025.jpg")}/>
       Wir haben mit dem Frühtrachthonig an einer Honigprämierung teilgenommen.
       Eine Honigprämierung ist eine jährliche Verleihung von Auszeichnungen für Honig, bei der Honigproben von Imkern nach festgelegten Qualitätskriterien bewertet werden.
       Kriterien wie Sauberkeit, Geschmack, Geruch, Wassergehalt und Invertaseaktivität werden auf einer Skala von 0 bis 5 bewertet. 
       Mit den Werten von 17,1% Wassergehalt, der elektrischen Leitfähigkeit von 0,20, dem Invertasewert von 169,1 U/kg und einem HMF von 1,9 g/kg wurde unser Frühtrachthonig am 09.11.2025 mit der <strong><u>Leistungsmedaille in Silber </u></strong>ausgezeichnet.
 
       </p>
+      {fullscreenSrc && (
+          <div className="fullscreen-overlay" onClick={() => setFullscreenSrc(null)}>
+            <span className="close-button">&times;</span>
+            <img src={fullscreenSrc} alt="Vollbild" className="fullscreen-image" />
+          </div>
+        )}
     </HauptLayout>
   );
 }
